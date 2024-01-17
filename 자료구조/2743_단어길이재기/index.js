1a908a2a587ed1d0c5016e0e3bc800667f383101
hyeri-woo <woohyeri0525@gmail.com>
2024-01-17 10:44:20 +0900

    solved: 2743_단어길이재기

---
+++ 자료구조/2743_단어길이재기/index.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+// TODO: 제출 시 경로 변환 필수 ("/dev/stdin")
+const input = fs.readFileSync("test.txt").toString().trim().split("\n");
+
+// 풀이
+function solution(input) {
+  return input[0].length;
+}
+
+console.log(solution(input));
