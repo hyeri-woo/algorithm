1b53c749df9cca50b6014504f09d7356141755b6
hyeri-woo <woohyeri0525@gmail.com>
2024-02-06 10:35:31 +0900

    solved: 11055_가장큰증가하는부분수열

---
+++ 다이나믹프로그래밍/11055_가장큰증가하는부분수열/index.js
@@ -0,0 +1,19 @@
+const fs = require("fs");
+// TODO: 제출 시 경로 변환 필수 ("/dev/stdin")
+const input = fs.readFileSync("test.txt").toString().trim().split("\n");
+
+// 풀이
+function solution(input) {
+  const numbers = input[1].split(" ").map((n) => parseInt(n));
+  const dp = [...numbers];
+  for (let i = 1; i < numbers.length; i++) {
+    for (let j = 0; j < i; j++) {
+      if (numbers[j] < numbers[i]) {
+        dp[i] = Math.max(dp[i], dp[j] + numbers[i]);
+      }
+    }
+  }
+  return Math.max(...dp);
+}
+
+console.log(solution(input));
