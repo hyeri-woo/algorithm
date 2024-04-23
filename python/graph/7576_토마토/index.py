3acf091ef95b970d8351834a0b9d2ef469e92268
hyeri-woo <woohyeri0525@gmail.com>
2024-04-23 15:57:50 +0900

    solved: 7576_토마토 - better version

--- python/graph/7576_토마토/index.py
+++ python/graph/7576_토마토/index.py
@@ -1,42 +1,41 @@
 from collections import deque
-f = open('test5.txt')
-n, m = map(int, f.readline().split())
-tomatos = [list(map(int, f.readline().split())) for _ in range(m)]
+f = open('test1.txt')
+m, n = map(int, f.readline().split()) # 가로, 세로
+graph = [list(map(int, f.readline().split())) for _ in range(n)]
 
-def has_zero(matrix):
-    return any(0 in row for row in matrix)
+# 익은 토마토의 위치 queue에 추가
+queue = deque()
+for i in range(n):
+    for j in range(m):
+        if graph[i][j] == 1:
+            queue.append((i, j))
 
-def find_indices_with_value(array, value):
-    indices = []
-    for i in range(len(array)):
-        for j in range(len(array[i])):
-            if array[i][j] == value:
-                indices.append((i, j))
-    return indices
-
-def bfs(graph, visited, start):
+# BFS 함수 정의
+def bfs():
     directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
-    for x, y in start:
-        visited[x][y] = True
-    queue = deque([start])
-    count = 0
     while queue:
-        next_nodes = queue.popleft()
-        temp_arr = []
-        for x, y in next_nodes:
-            for dx, dy in directions:
-                nx, ny = x + dx, y + dy
-                if 0 <= nx < m and 0 <= ny < n:
-                    if not visited[nx][ny] and graph[nx][ny] != -1:
-                        temp_arr.append((nx, ny))
-                        visited[nx][ny] = True
-                        graph[nx][ny] += 1
-        if len(temp_arr) > 0:
-            queue.append(temp_arr)
-            count += 1
-    return count
+        x, y = queue.popleft()
+        dx = [-1, 1, 0, 0]
+        dy = [0, 0, -1, 1]
+        for dx, dy in directions:
+            nx, ny = x + dx, y + dy
+            # 상하좌우에 익지 않은 토마토(0)가 있으면 1을 더해 몇 번째인지 세주고
+            if 0 <= nx < n and 0 <= ny < m and graph[nx][ny] == 0:
+                graph[nx][ny] = graph[x][y] + 1
+                queue.append((nx, ny)) # 큐에 새로운 토마토 위치 추가
 
+# BFS; 토마토 익히기 시작
+bfs()
 
-visited = [[False] * n for _ in range(m)]
-count = bfs(tomatos, visited, find_indices_with_value(tomatos, 1))
-print(-1 if has_zero(tomatos) else count)
+# bfs 종료 후
+day = 0
+for row in graph:
+    for i in row:
+        if i == 0:  # 익지 않은 토마토가 있으면(=토마토가 모두 익지 못하는 상황) -1 출력
+            print(-1)
+            exit() # 다음 행의 여부와 관계 없이 -1만 출력해야하므로 종료시킴
+    else: # 그래프에서 가장 큰 값이 마지막으로 익은 토마토임 → 한 줄씩 최댓값을 day로 갱신하며 최댓값 찾기
+        day = max(day, max(row)) 
+
+# 처음 시작을 0이 아닌 1부터 했으므로 -1을 해야 날짜를 구할 수 있음 
+print(day-1)
