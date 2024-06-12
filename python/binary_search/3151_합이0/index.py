f742c0cfdbd4174c34f03a71756f1cf6a3303a3f
hyeri-woo <woohyeri0525@gmail.com>
2024-06-12 22:52:39 +0900

    solved: 3151 합이0

--- python/binary_search/3151_합이0/index.py
+++ python/binary_search/3151_합이0/index.py
@@ -1,4 +1,5 @@
 import sys
+from bisect import bisect_left
 # input = sys.stdin.readline
 f = open('test1.txt')
 input = f.readline
@@ -6,40 +7,25 @@ n = int(input())
 numbers = list(map(int, input().split()))
 numbers.sort()
 
-def search(start, end):
-    
-    while start <= end:
-        mid = (start + end) // 2
-        
+answer = 0
 
-for i in range(n-2):
-    for j in range(i+1, n-1):
-        target = -(numbers[i] + numbers[j])
-        print(numbers[i], numbers[j], target)
+for i in range(n - 2):  # 3명을 고르기 위해 0 ~ n-3
+    # 이분 탐색
+    start = i + 1
+    end = n - 1
+    while start < end:
+        result = numbers[i] + numbers[start] + numbers[end]
+        if result > 0:  # 0보다 크면 end 감소
+            end -= 1
+        else:           # 0보다 작거나 같으면 start 증가
+            if result == 0:
+                # start에 위치한 수와, end에 위치한 수가 같으면 start와 end 사이의 모든 조합이 합이 0이므로, end-start
+                if numbers[start] == numbers[end]:
+                    answer += end - start
+                # 다르다면, bisect_left를 사용하여, end와 같은 값을 가지는 첫번째 인덱스를 찾아, 그 범위 내의 경우의 수를 더함
+                else:
+                    idx = bisect_left(numbers, numbers[end])
+                    answer += end - idx + 1
+            start += 1
 
-# def count_zero_sum_triplets(arr):
-#     # 정렬된 배열
-#     arr.sort()
-#     n = len(arr)
-#     count = 0
-
-#     for i in range(n - 2):
-#         for j in range(i + 1, n - 1):
-#             target = -(arr[i] + arr[j])
-#             # 이분 탐색
-#             left, right = j + 1, n - 1
-#             while left <= right:
-#                 mid = (left + right) // 2
-#                 if arr[mid] == target:
-#                     count += 1
-#                     break
-#                 elif arr[mid] < target:
-#                     left = mid + 1
-#                 else:
-#                     right = mid - 1
-
-#     return count
-
-# # 예시 입력
-# A = [-1, 2, -1, 1, 0, -4]
-# print(count_zero_sum_triplets(A))  # 결과는 2여야 합니다.
+print(answer)
