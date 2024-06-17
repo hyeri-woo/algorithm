38e4f269b5b8235fca4a184b6b9ba8f53f27a7fb
hyeri-woo <woohyeri0525@gmail.com>
2024-06-18 00:06:00 +0900

    solved: 2343 기타레슨

--- python/binary_search/2343_기타레슨/index.py
+++ python/binary_search/2343_기타레슨/index.py
@@ -4,5 +4,32 @@ f = open('test1.txt')
 input = f.readline
 n, m = map(int, input().split())
 lectures = list(map(int, input().split()))
+lectures.sort()
 
-print(n, m, lectures)
+def is_valid_length(max_length):
+    partial_sum = 0
+    count = 1
+    for l in lectures:
+        if l + partial_sum > max_length:
+            partial_sum = 0
+            count += 1
+        partial_sum += l
+    if count <= m:
+        return True
+    return False
+
+def search(start, end):
+    result = 0
+    while start <= end:
+        mid = (start + end) // 2
+        if is_valid_length(mid):
+            result = mid
+            end = mid - 1
+        else:
+            start = mid + 1
+    return result
+
+start = max(lectures)
+end = sum(lectures)
+
+print(search(start, end))
