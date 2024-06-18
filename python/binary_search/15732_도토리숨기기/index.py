5c2c58135ef83d0804ddeac431b843f427fd54bb
hyeri-woo <woohyeri0525@gmail.com>
2024-06-18 10:46:53 +0900

    solved: 15732 도토리숨기기

--- python/binary_search/15732_도토리숨기기/index.py
+++ python/binary_search/15732_도토리숨기기/index.py
@@ -4,4 +4,25 @@ f = open('test1.txt')
 input = f.readline
 n, k, d = map(int, input().split())
 rules = [list(map(int, input().split())) for _ in range(k)]
-print(n, k, d, rules)
+
+def count_acorn (box_num):
+    total_acorns = 0
+    for a, b, c in rules:
+        if a <= box_num:
+            total_acorns += (min(b, box_num) - a) // c + 1
+    return total_acorns
+
+def search(start, end):
+    while start < end:
+        mid = (start + end) // 2
+        count = count_acorn(mid)
+        if count >= d:
+            end = mid
+        else:
+            start = mid + 1
+    return start
+
+start = 1
+end = n
+
+print(search(start, end))
